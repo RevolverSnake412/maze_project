47a0478e3aceb953f39063f1d19c2a51b74361af
RevolverSnake412 <oussa@hotmail.fr>
2024-07-28 03:24:05 +0100

    update

---
+++ CMakeFiles/maze_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maze_project.dir/src/main.c.o"
+  "CMakeFiles/maze_project.dir/src/main.c.o.d"
+  "maze_project"
+  "maze_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/maze_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
